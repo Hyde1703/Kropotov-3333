80d7959d57a3315a459f80f432b7192a2d2ecc89
Hyde1703 <146088337+Hyde1703@users.noreply.github.com>
2025-06-23 10:38:51 +0300

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO todolist.accounts (`username`, `passhash`) VALUES ('test1', '4813494d137e1631bba301d5acab6e7bb7aa74ce1185d456565ef51d737677b2');
+INSERT INTO todolist.accounts (`username`, `passhash`) VALUES ('test2', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92');
+INSERT INTO todolist.accounts (`username`, `passhash`) VALUES ('test3', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08');
+
+INSERT INTO todolist.items (`user_id`, `text`) VALUES (1, '!@$%^&*');
+INSERT INTO todolist.items (`user_id`, `text`) VALUES (2, 'xcxcxcxcx');
+INSERT INTO todolist.items (`user_id`, `text`) VALUES (3, 'd?>d,d,dzxcxzc');
+INSERT INTO todolist.items (`user_id`, `text`) VALUES (1, 'ds sd zxzczzxz');
+INSERT INTO todolist.items (`user_id`, `text`) VALUES (3, '16*28tdgc');
+INSERT INTO todolist.items (`user_id`, `text`) VALUES (1, '**8dhdhc8***');
+INSERT INTO todolist.items (`user_id`, `text`) VALUES (2, '12123  sjfspafj <');
+INSERT INTO todolist.items (`user_id`, `text`) VALUES (2, 'skdsaudhaoh27du(.');
